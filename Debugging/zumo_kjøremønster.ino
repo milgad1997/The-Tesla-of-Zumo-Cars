6bb205e0b4061b426dcfd70d8997651bb175864c
espi3000 <60798059+espi3000@users.noreply.github.com>
2020-03-28 10:47:55 +0100

    Create zumo_kjøremønster.ino

---
+++ Debugging/zumo_kjøremønster.ino
@@ -0,0 +1,15 @@
+#include <Zumo32U4.h>               //Importerer Zumo-biblioteket
+
+Zumo32U4Motors motors;              //Oppretter instans av motorane
+Zumo32U4ButtonA buttonA;            //Oppretter instans av knapp A
+
+
+void setup() {
+  buttonA.waitForButton();
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
