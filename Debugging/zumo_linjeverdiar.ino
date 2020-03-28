b3e12240e055b8a7b25e1da791637907c2c2e6d7
espi3000 <60798059+espi3000@users.noreply.github.com>
2020-03-28 11:31:20 +0100

    Program for testing av linjeverdiar

--- Debugging/zumo_linjeverdiar.ino
+++ Debugging/zumo_linjeverdiar.ino
@@ -1,1 +1,73 @@
+#include <Zumo32U4.h>               //Importerer Zumo-biblioteket
 
+Zumo32U4Motors motors;              //Oppretter instans av motorane
+Zumo32U4LineSensors lineSensors;    //Oppretter instans av linjesensorane
+Zumo32U4ButtonA buttonA;            //Oppretter instans av knapp A
+
+
+unsigned int lineSensorValues[5];   //Verdien til kvar linjesensor
+
+
+void setup() 
+{
+    lineSensors.initFiveSensors();                  //Starter 5-linjesensorkonfigurasjonen
+
+    Serial.begin(9600);
+    while(!Serial);
+
+    Serial.println("Press A to calibrate\n");
+
+    buttonA.waitForButton();
+    delay(1000);
+
+    for (int i = 0; i < 1000; i++) {
+        Serial.print("Calibrating...\t");
+        Serial.println(i);
+
+        lineSensors.calibrate();
+
+        delay(1);
+    }
+
+    Serial.println("Done!\n");
+    Serial.println("Press A to test\n");
+
+    buttonA.waitForButton();
+    delay(1000);
+}
+
+
+void loop()
+{
+    Serial.print("Alle: ");
+    Serial.print(lineSensors.readLine(lineSensorValues));
+    Serial.print("\t");
+
+    Serial.print("1: ");
+    Serial.print(lineSensorValues[0]);
+    Serial.print("\t");
+
+    Serial.print("2: ");
+    Serial.print(lineSensorValues[1]);
+    Serial.print("\t");
+
+    Serial.print("3: ");
+    Serial.print(lineSensorValues[2]);
+    Serial.print("\t");
+
+    Serial.print("4: ");
+    Serial.print(lineSensorValues[3]);
+    Serial.print("\t");
+
+    Serial.print("5: ");
+    Serial.print(lineSensorValues[4]);
+    Serial.print("\t");
+
+    if (buttonA.isPressed()) {
+        delay(2000);
+        buttonA.waitForButton();
+        delay(1000);
+    }
+
+    delay(200);
+}
