fd77073750a8c8e00eb08237da24a50b95417dd0
haavardok <havris97@gmail.com>
2020-03-31 10:10:43 +0200

    New try: Added init interock in followLine() and followObject(). Fixed bugs as pointed out in comments on last commit.

--- Zumo/main.ino
+++ Zumo/main.ino
@@ -16,17 +16,17 @@ class SelfDriving
 {
     private:
 
-        static bool lineInit;                               //Static bool to save variable value through program
-        static bool proxInit;                       
-
-
+        static bool lineInit;                               //Static bool to save variable value through program 
+        static bool proxInit;                        
+ 
+ 
     public:
 
         void calibrateSensors() 
         {
-            lineInit = false;                               //Linesensors is now configured, no need to do it again
-            proxInit = true;                                //unless prox. sensors config has been run
-
+            lineInit = false;                               //Linesensors is now configured, no need to do it again 
+            proxInit = true;                                //unless prox. sensors config has been run 
+ 
             lineSensors.initFiveSensors();                  //Starter 5-linjesensorkonfigurasjonen
 
             for (int t = 0; t <= 200; t++) {                //Varer i 4000ms der t er tid i ms
@@ -42,13 +42,13 @@ class SelfDriving
 
         void followLine(int value, bool fastMode, int batteryLevel)
         {
-            if (lineInit && !proxInit) {
-                lineSensors.initFiveSensors();
-
-                lineInit = false;                            //Sensor init interlock
-                proxInit = true;
-            }
-
+            if (lineInit && !proxInit) { 
+                lineSensors.initFiveSensors(); 
+ 
+                lineInit = false;                            //Sensor init interlock 
+                proxInit = true; 
+            } 
+ 
             int leftSpeed;
             int rightSpeed;
 
@@ -83,11 +83,11 @@ class SelfDriving
 
         void followObject()                             //Follow an object within prox. sensor range (~30-40 cm) as it would follow lines on ground
         {
-            if (!lineInit && proxInit) {
+            if (!lineInit && proxInit) { 
                 proxSensors.initFrontSensor();          //Need to call this function in order to use the front prox. sensor
                                                         //Can only use front when full line sensor array in use, ref. p. 20 in Pololu User Guide
-                proxInit = false;                       //Keep init from running more than once
-                lineInit = true;                        //Sensor init interlock
+                proxInit = false;                       //Keep init from running more than once 
+                lineInit = true; 
             }                       
 
             int driveSpeed = 200;
@@ -114,11 +114,11 @@ class SelfDriving
 
             if (((leftReading + rightReading) / 2) == 2) motors.setSpeeds(driveSpeed, driveSpeed);   //Object is close to front of Zumo and directly in front of it, drive towards object
 
-            else if (leftReading >= 4 || rightReading >= 4) motors.setSpeeds(0, 0);                  //Object must be very close to an object, stop motors to prevent damage as preventive measure
+            else if (leftReading >= 4 || rightReading >= 4) motors.setSpeeds(0, 0);                  //Object must be very close to an object, stop motors to prevent damage as preventive measure 
 
-            else if (leftReading > rightReading) motors.setSpeeds(driveSpeed, turnSpeed);            //Object is to the left, turn left
+            else if (leftReading > rightReading) motors.setSpeeds(driveSpeed, turnSpeed);            //Object is to the left, turn left 
 
-            else if (leftReading < rightReading) motors.setSpeeds(turnSpeed, driveSpeed);            //Objct is to the right, turn right
+            else if (leftReading < rightReading) motors.setSpeeds(turnSpeed, driveSpeed);            //Objct is to the right, turn right 
         }
 };
 
