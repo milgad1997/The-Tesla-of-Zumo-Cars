0aa018129aff15dd40ac063ec3d051ffae53c166
espi3000 <60798059+espi3000@users.noreply.github.com>
2020-04-09 14:48:02 +0200

    Har fått til webserveren, men veit ikkje korleis eg sender andre typar enn char* enda.

--- ESP32/sensornode.ino
+++ ESP32/sensornode.ino
@@ -3,19 +3,28 @@
 
 #include <WiFi.h>
 #include <WiFiClient.h>
+#include <WebServer.h>
 #include <BlynkSimpleEsp32.h>
 //Include sensor- and servo-libraries here
 
-char auth[] = "YourAuthToken";
-char ssid[] = "YourNetworkName";
-char pass[] = "YourPassword";
+const char* AUTH = "";
+const char* SSID = "";
+const char* PASS = "";
+const char* HTML = 
+    "<!DOCTYPE html>\
+    <html>\
+      <body>\
+        <h1>Gruppe 2: Web server</h1>\
+      </body>\
+    </html>";
+
 
 BlynkTimer timer;
+WebServer server;   //Port is 80 as default
 
 
 
-class Sensor
-{
+class Sensor {
     protected:
 
         byte pin;
@@ -33,8 +42,7 @@ class Sensor
 };
 
 
-class TMP36TemperatureSensor: public Sensor
-{
+class TMP36TemperatureSensor: public Sensor {
     public:
     
         TMP36TemperatureSensor(byte pinNumber) {
@@ -45,8 +53,7 @@ class TMP36TemperatureSensor: public Sensor
 };
 
 
-class HCSR04UltrasonicSensor: public Sensor
-{
+class HCSR04UltrasonicSensor: public Sensor {
     private:
 
         byte trigger;
@@ -65,8 +72,7 @@ class HCSR04UltrasonicSensor: public Sensor
 };
 
 
-class VL6180XTimeOfFlight
-{
+class VL6180XTimeOfFlight {
     private:
 
     public:
@@ -76,63 +82,59 @@ class VL6180XTimeOfFlight
 
 
 
-
 // This function sends Arduino's up time every second to Virtual Pin (5).
 // In the app, Widget's reading frequency should be set to PUSH. This means
 // that you define how often to send data to Blynk App.
-void myTimerEvent1()
-{
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V5, millis() / 1000);
+void timerEvent1() {
+    // You can send any value at any time.
+    // Please don't send more that 10 values per second.
+    Blynk.virtualWrite(V5, millis() / 1000);
 }
 
 
-void myTimerEvent2()
-{
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V5, millis() / 60000);
+void timerEvent2() {
+    // You can send any value at any time.
+    // Please don't send more that 10 values per second.
+    Blynk.virtualWrite(V5, millis() / 60000);
 }
 
 
 // This function will be called every time widget
 // in Blynk app writes values to the Virtual Pin V1
-BLYNK_WRITE(V1)
-{
-  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
-
-  // process received value
+BLYNK_WRITE(V1) {
+    int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+    // process received value
 }
 
 
 // This function is called when there is a widget
 // which is requesting data from Virtual Pin V5
-BLYNK_READ(V5)
-{
-  // This command writes Arduino's uptime in seconds to Virtual Pin (5)
-  Blynk.virtualWrite(V5, millis() / 1000);
+BLYNK_READ(V5) {
+    // This command writes Arduino's uptime in seconds to Virtual Pin (5)
+    Blynk.virtualWrite(V5, millis() / 1000);
 }
 
 
-void setup()
-{
-  // Debug console
-  Serial.begin(9600);
-
-  Blynk.begin(auth, ssid, pass);
-  // You can also specify server:
-  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
-  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
-
-  // Setup a function to be called every second and minute
-  timer.setInterval(1000L, myTimerEvent1);
-  timer.setInterval(60000L, myTimerEvent2);
+void handleRoot() {
+    server.send(200, "text/html", HTML);                            //Code 200, display HTML code.
 }
 
 
-void loop()
-{
-  Blynk.run();
-  timer.run(); // Initiates BlynkTimer
+void setup() {
+    Serial.begin(115200);                                           // Debug console
+
+    Blynk.begin(AUTH, SSID, PASS, IPAddress(91,192,221,40), 8080);
+    server.begin();                                                 //Initiates web server.
+    server.on("/", handleRoot);                                     //Manage HTTP request and run handleRoot() when "IP"/ is searched in browser.
+    Serial.println("HTTP server started");
+
+    timer.setInterval(1000L, timerEvent1);                          // Setup a function to be called every second and minute
+    timer.setInterval(60000L, timerEvent2);
+}
+
+
+void loop() {
+    Blynk.run();
+    timer.run();                                                    // Initiates BlynkTimer
+    server.handleClient();                                          //Checking web server to manage occurring events
 }
